6f7fbd09274b6fca8ee923a1f96ba35fedc5debe
#$(h)@h!D <101791916+Shelltroecnec@users.noreply.github.com>
2022-04-27 11:28:52 +0000

    UART header file Added

---
+++ 8051_basic_interfacing/Bluetooth/uart_header.h
@@ -0,0 +1,47 @@
+void delay(int t);
+unsigned char rx(void);
+void serial_init(void);
+void display(unsigned char *p);
+unsigned int i;
+void new_line(void);
+unsigned char re;
+
+void tx(unsigned char b)
+{
+SBUF=b;
+while(TI==0);
+TI=0;
+}
+unsigned char rx(void)
+{
+	while(RI==0);
+	RI=0;
+	re=SBUF;
+	return re;
+}
+void delay(int t)
+{
+int i;
+for(i=0;i<=1275*t;i++);
+}
+void serial_init()
+{
+TMOD = 0x20;
+REN = 1;
+SCON = 0x50;
+TH1 = 0xFD;
+TR1 = 1;
+}
+void display(unsigned char *p)
+{
+	int i;
+	for(i=0; *p!= '\0';i++)
+	{
+	tx(*p++);
+	}
+}
+void new_line(void)
+{
+	tx(0x0D); //
+	tx(0x0A); //
+}
